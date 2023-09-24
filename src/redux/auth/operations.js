4c1b88ac09cf16c2c2998595f0ff1755bdd6b5ad
Bondana0 <120127799+Bondana0@users.noreply.github.com>
2023-09-24 23:45:44 +0300

    fix

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
-axios.defaults.baseURL = 'https://64e1d8deab00373588187553.mockapi.io/api/v1/';
+axios.defaults.baseURL = 'https://connections-api.herokuapp.com';
 
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
